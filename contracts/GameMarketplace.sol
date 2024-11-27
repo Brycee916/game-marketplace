48f4f49c4e9aac9be993d7cd2c3f9766deb81ab4
MachuuJunio <Matthewbjunio@gmail.com>
2024-11-26 23:14:32 -0800

    Added game library to the function list

--- contracts/GameMarketplace.sol
+++ contracts/GameMarketplace.sol
@@ -12,6 +12,7 @@ contract GameMarketplace {
 
     mapping(uint => Game) public games;
     uint public gameCount;
+    mapping(address => uint) public userGameCount; // Tracks the number of games owned by each user
 
     event GamePurchased(uint gameId, address buyer);
     event GameTransferred(uint gameId, address from, address to);
@@ -20,6 +21,7 @@ contract GameMarketplace {
     function addGame(string memory title, uint price) public {
         gameCount++;
         games[gameCount] = Game(gameCount, title, price, true, msg.sender);
+        userGameCount[msg.sender]++; // Increment the creator's game count
     }
 
     // Purchase a game from the marketplace
@@ -28,12 +30,16 @@ contract GameMarketplace {
 
         // Check if the game exists
         require(game.exists, "Game does not exist");
-        
+
         // Check if the correct amount of Ether is sent
         require(msg.value == game.price, "Incorrect price sent");
 
         // Transfer the Ether to the game's owner
         payable(game.owner).transfer(msg.value);
+
+        // Adjust game ownership counts
+        userGameCount[game.owner]--; // Decrease previous owner's count
+        userGameCount[msg.sender]++; // Increase buyer's count
 
         // Transfer ownership to the buyer
         address previousOwner = game.owner;
@@ -44,7 +50,7 @@ contract GameMarketplace {
         emit GameTransferred(gameId, previousOwner, msg.sender);
     }
 
-     // Transfer ownership of a game
+    // Transfer ownership of a game
     function transferGame(uint gameId, address newOwner) public {
         Game storage game = games[gameId];
 
@@ -53,6 +59,10 @@ contract GameMarketplace {
 
         // Check that the caller is the current owner
         require(game.owner == msg.sender, "You are not the owner of this game");
+
+        // Adjust game ownership counts
+        userGameCount[msg.sender]--; // Decrease sender's count
+        userGameCount[newOwner]++;   // Increase recipient's count
 
         // Update the game's owner
         address previousOwner = game.owner;
@@ -70,6 +80,32 @@ contract GameMarketplace {
         require(game.exists, "Game does not exist");
 
         return (game.title, game.price, game.owner);
+    }
+
+    // List all game titles in a user's library
+    function getUserLibrary(address user) public view returns (string[] memory) {
+        uint userGameCounter = 0;
+
+        // Count how many games the user owns
+        for (uint i = 1; i <= gameCount; i++) {
+            if (games[i].owner == user) {
+                userGameCounter++;
+            }
+        }
+
+        // Create an array to hold the titles of the user's games
+        string[] memory userGameTitles = new string[](userGameCounter);
+        uint index = 0;
+
+        // Populate the array with the user's game titles
+        for (uint i = 1; i <= gameCount; i++) {
+            if (games[i].owner == user) {
+                userGameTitles[index] = games[i].title;
+                index++;
+            }
+        }
+
+        return userGameTitles;
     }
 
     // This could be the deployer's address or any specific address that you want to receive the funds
